2ba0935b350671210767351eee919512ed5a0c18
Alldlights <emekachris202@gmail.com>
2023-03-13 10:23:22 -0700

    lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -9,7 +9,7 @@
  * Description: singly linked list node structure
  * for project
  */
-typedef struct list_int_s
+typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
@@ -18,7 +18,7 @@ typedef struct list_int_s
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+void reverse_listint(listint_t **head);
 int is_palindrome(listint_t **head);
 
-endif /* LISTS_H */
+#endif /* LISTS_H */
