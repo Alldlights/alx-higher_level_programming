3848fe20d5064dd2724834f2570df15602ed7844
Alldlights <emekachris202@gmail.com>
2024-03-19 07:47:38 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,8 +3,8 @@
 
 def multiple_returns(sentence):
     lenght = len(sentence)
-    if lenght < 0:
-        return "None"
+    if lenght == 0:
+        return None
     else:
         f_s = sentence[0]
         return (lenght, f_s)
