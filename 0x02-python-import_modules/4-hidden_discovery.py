75d007da04171346e18444168a46bbc61cd81b6e
Alldlights <emekachris202@gmail.com>
2024-03-20 09:51:35 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3.8.x
+#!/usr/bin/python3.8
 import hidden_4
 if __name__ == "__main__":
     names = dir(hidden_4)
     for name in sorted(names):
-        if name[0:2] != ("__"):
+        if name[0:2] != "__":
             print(name)
